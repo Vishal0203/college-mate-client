c53733433a111aa9a8a9e4f7f43059ecdc429c30
Vishal Sharma <vish.sharma0203@gmail.com>
2016-06-29 02:54:34 +0530

    initial commit iOS

---
+++ iOS/collegemate/collegemate/HomeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeViewController.swift
+//  collegemate
+//
+//  Created by Vishal Sharma on 29/06/16.
+//  Copyright © 2016 ToDevs. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var navMenu: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if self.revealViewController() != nil {
+            navMenu.target = self.revealViewController()
+            navMenu.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
